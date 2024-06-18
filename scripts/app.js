0c095f1509fc8d9248964b5048ba6bc3d3c14cf4
mkheck13 <mkheck13@gmail.com>
2024-06-18 09:03:57 -0700

    chuck norris

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+// It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+
+// It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+// Go show some truth who's boss!
+
+
+
+// So I stumbled across this one. It made me laugh so I figured I'd do it.
+// I just have to return a false without ever using the word false.
+// to do that we will just use a ! in front of true which makes it false
+
+function ifChuckSaysSo(){
+    return !true;
+}
